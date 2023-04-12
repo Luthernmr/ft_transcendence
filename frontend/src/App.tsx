483bad1341d916e346c77c7d1c7c3d3dc9d3c288
root <nemorluther@gmail.com>
2023-04-12 12:18:29 +0000

    add sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Root from './routes/root'
+import React from 'react'
+import { Progress } from '@chakra-ui/react'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+import SidebarWithHeader from './component/Side'
 import Account from './account/Accout'
 import Chat from './chat/Chat'
 import Pong from './pong/Pong'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <BrowserRouter>
+    <ChakraProvider>
+	<SidebarWithHeader>
+	<BrowserRouter>
       <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/pong" element={<Pong />} />
+        <Route path="/Home" element={<App />} />
+        <Route path="/Account" element={<Account />} />
+        <Route path="/Chat" element={<Chat />} />
+        <Route path="/Pong" element={<Pong />} />
       </Routes>
     </BrowserRouter>
-  );
+	</SidebarWithHeader>
+    </ChakraProvider>
+  )
 }
-
-export default App;
+export default App
