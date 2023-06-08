a6b13b5fad2c0c9abec05d0e5dcbf2ee0c1fc386
Azathothep <felixbelthoise@yahoo.fr>
2023-06-08 10:35:05 +0200

    rm dist & corrected 212 strategy

--- backend/dist/auth/auth42.guard.d.ts
+++
@@ -1,4 +0,0 @@
-declare const auth42Guard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class auth42Guard extends auth42Guard_base {
-}
-export {};
--- backend/dist/auth/auth42.strategy.d.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-declare const auth42Strategy_base: new (...args: any[]) => any;
-export declare class auth42Strategy extends auth42Strategy_base {
-    constructor();
-    validate(access_token: string, refresh_token: string, user: any): any;
-}
-export {};
