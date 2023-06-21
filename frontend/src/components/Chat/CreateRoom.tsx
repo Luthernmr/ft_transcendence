9be1bc6363ce3b7bd0cb3e1b1fab1f363c17b986
Arnaud <acroisie@student.42lyon.fr>
2023-06-21 16:25:09 +0200

    update

--- frontend/src/components/Chat/CreateRoom.tsx
+++ frontend/src/components/Chat/CreateRoom.tsx
@@ -1,18 +1,27 @@
-import { ArrowBackIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon, CheckIcon } from "@chakra-ui/icons";
 import {
+  Avatar,
+  AvatarBadge,
+  Badge,
+  Box,
   Button,
   Flex,
   Heading,
   IconButton,
   Input,
-  Select,
-  Text,
   Spacer,
-  Box,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { chatSocket, userSocket } from "../../sockets/sockets";
-import { User } from "../Social/AllUserItem";
+
+interface User {
+  id: number;
+  nickname: string;
+  imgPdp: string;
+  isOnline: boolean;
+}
 
 interface Room {
   name: string;
@@ -24,36 +33,15 @@ interface CreateRoomProps {
 }
 
 const CreateRoom: React.FC<CreateRoomProps> = ({ setShowCreateRoom }) => {
-  const [room, setRoom] = useState<Room[]>([]);
   const [roomName, setRoomName] = useState("");
-  const [members, setMembers] = useState<User[]>([]);
   const [allUsers, setAllUsers] = useState<User[]>([]);
-  const [selectedUser, setSelectedUser] = useState<string>("");
+  const [members, setMembers] = useState<User[]>([]);
 
   useEffect(() => {
     userSocket.on("userList", (data) => {
       setAllUsers(data);
     });
     userSocket.emit("getAllUsers");
-  }, []);
-
-  useEffect(() => {
-    if (selectedUser !== "") {
-      const userToAdd = allUsers.find(
-        (user) => user.id.toString() === selectedUser
-      );
-      if (userToAdd) {
-        setMembers([...members, userToAdd]);
-        setSelectedUser("");
-      }
-    }
-  }, [selectedUser]);
-
-  useEffect(() => {
-    chatSocket.on("createRoom", (data) => {
-      console.log("createRoom", data);
-      setRoom(data);
-    });
   }, []);
 
   function handleCreate(e: any) {
@@ -64,6 +52,14 @@ const CreateRoom: React.FC<CreateRoomProps> = ({ setShowCreateRoom }) => {
     }
   }
 
+  function handleAddMember(user: User) {
+    const memberExists = members.find((m) => m.id === user.id);
+    if (!memberExists) {
+      setMembers([...members, user]);
+    } else {
+      setMembers(members.filter((m) => m.id !== user.id));
+    }
+  }
   return (
     <Flex
       borderRadius={"md"}
@@ -83,42 +79,71 @@ const CreateRoom: React.FC<CreateRoomProps> = ({ setShowCreateRoom }) => {
           Create Room
         </Heading>
       </Flex>
-      <Box mb={4}>
-        <Input
-          value={roomName}
-          onChange={(e) => setRoomName(e.target.value)}
-          placeholder="Type the name of the room..."
-          borderRadius="md"
-        />
-      </Box>
 
-      <Box mb={4}>
-        <Select
-          placeholder="Select group members"
-          value={selectedUser}
-          onChange={(e) => setSelectedUser(e.target.value)}
-        >
-          {allUsers.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.nickname}
-            </option>
-          ))}
-        </Select>
-      </Box>
+      <Input
+        value={roomName}
+        onChange={(e) => setRoomName(e.target.value)}
+        placeholder="Type the name of the room..."
+        borderRadius="md"
+        mb={4}
+      />
 
-      <Flex direction="column" mb={4}>
-        {members.map((member) => (
-          <Text key={member.id}>{member.nickname}</Text>
+      <Text mb={2} fontWeight="semibold">
+        Click on a user to add them to the room:
+      </Text>
+
+      <Box
+        overflowY="scroll"
+        border="1px solid"
+        borderColor="gray.200"
+        borderRadius="md"
+        p={2}
+        mb={4}
+        maxH="200px"
+      >
+        {allUsers.map((user) => (
+          <Flex
+            key={user.id}
+            alignItems={"center"}
+            _hover={{ bg: "gray.200", cursor: "pointer" }}
+            padding={"2"}
+            w={"100%"}
+            borderRadius={"8"}
+            onClick={() => handleAddMember(user)}
+          >
+            <Avatar size="sm" src={user.imgPdp}>
+              <AvatarBadge boxSize="1em" bg={user.isOnline ? "green.500" : "tomato"} />
+            </Avatar>
+            <Box ml="2">
+              <Flex alignItems="center">
+                <Heading size="sm">{user.nickname}</Heading>
+                <Badge ml="1" colorScheme={user.isOnline ? "green" : "red"}>
+                  {user.isOnline ? "Online" : "Offline"}
+                </Badge>
+              </Flex>
+            </Box>
+            {members.find((m) => m.id === user.id) && <CheckIcon ml="auto" color="green.500" />}
+          </Flex>
         ))}
-      </Flex>
+      </Box>
+
+      <Text mb={2} fontWeight="semibold">
+        Selected members:
+      </Text>
+
+      <Stack direction="row" spacing={2} overflowX="auto" mb={4}>
+        {members.map((user) => (
+          <Avatar key={user.id} size="sm" src={user.imgPdp}>
+            <AvatarBadge boxSize="1em" bg={user.isOnline ? "green.500" : "tomato"} />
+          </Avatar>
+        ))}
+      </Stack>
 
       <Spacer />
 
-      <Box>
-        <Button width="full" colorScheme="teal" size="md" onClick={handleCreate}>
-          Create
-        </Button>
-      </Box>
+      <Button colorScheme="teal" size="md" w="100%" onClick={handleCreate}>
+        Create
+      </Button>
     </Flex>
   );
 };
