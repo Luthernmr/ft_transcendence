b79a39e6776c274a7a545c1ac45533238634eec0
Arnaud <acroisie@student.42lyon.fr>
2023-08-01 09:36:19 +0200

    update

--- frontend/src/components/Dashboard/Home.tsx
+++ frontend/src/components/Dashboard/Home.tsx
@@ -1,72 +1,66 @@
 import {
-	Button,
-	Flex,
-	Heading,
-	Image,
-	Stack,
-	Text,
-	useBreakpointValue,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { Link as RouteLink } from "react-router-dom";
 
 export default function Home() {
-	return (
-		<Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-			<Flex p={8} flex={1} align={"center"} justify={"center"}>
-				<Stack spacing={6} w={"full"} maxW={"lg"}>
-					<Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
-						<Text
-							as={"span"}
-							position={"relative"}
-							_after={{
-								content: "''",
-								width: "full",
-								height: useBreakpointValue({ base: "20%", md: "30%" }),
-								position: "absolute",
-								bottom: 1,
-								left: 0,
-								bg: "blue.400",
-								zIndex: -1,
-							}}
-						>
-							Welcome
-						</Text>
-						<br />{" "}
-						<Text color={"blue.400"} as={"span"}>
-							On our ft_transcendence
-						</Text>{" "}
-					</Heading>
-					<Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
-						Student project
-					</Text>
-					<Stack direction={{ base: "column", md: "row" }} spacing={4}>
-						<Button
-							as={RouteLink}
-							to="/register"
-							rounded={"full"}
-							bg={"blue.400"}
-							color={"white"}
-							_hover={{
-								bg: "blue.500",
-							}}
-						>
-							Register
-						</Button>
-						<Button as={RouteLink} to="/login" rounded={"full"}>
-							Login
-						</Button>
-					</Stack>
-				</Stack>
-			</Flex>
-			<Flex flex={1}>
-				<Image
-					alt={"Login Image"}
-					objectFit={"cover"}
-					src={"./src/assets/abowie.png"}
-				>
-
-				</Image>
-			</Flex>
-		</Stack>
-	);
+	const abowie = '/abowie.png';
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+            <Text
+              as={"span"}
+              position={"relative"}
+              _after={{
+                content: "''",
+                width: "full",
+                height: useBreakpointValue({ base: "20%", md: "30%" }),
+                position: "absolute",
+                bottom: 1,
+                left: 0,
+                bg: "blue.400",
+                zIndex: -1,
+              }}
+            >
+              Welcome
+            </Text>
+            <Text color={"blue.400"} as={"span"}>
+              On our ft_transcendence
+            </Text>{" "}
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+            Student project
+          </Text>
+          <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+            <Button
+              as={RouteLink}
+              to="/register"
+              rounded={"full"}
+              bg={"blue.400"}
+              color={"white"}
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              Register
+            </Button>
+            <Button as={RouteLink} to="/login" rounded={"full"}>
+              Login
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image alt={"Login Image"} objectFit={"cover"} src={abowie}></Image>
+      </Flex>
+    </Stack>
+  );
 }
