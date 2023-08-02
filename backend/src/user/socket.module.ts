1b21076a3e176b831d6cec66727aee62291ce8bf
acroisie <acroisie@student.42lyon.fr>
2023-08-02 08:59:07 +0200

    update

--- backend/src/user/socket.module.ts
+++ backend/src/user/socket.module.ts
@@ -8,12 +8,12 @@ import { GlobalGateway } from 'src/websockets/global.gateway';
 import { RoomService } from 'src/room/room.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from 'src/message/entities/message.entity';
-import { Mute } from 'src/room/entities/muted-user.entity';
+import { MutedUser } from 'src/room/entities/muted-user.entity';
 import { Room } from 'src/room/entities/room.entity';
 import { Friend } from 'src/social/friend.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Friend, Room, Message, Mute]),UserModule, AuthModule, FriendModule, PongModule],
+  imports: [TypeOrmModule.forFeature([Friend, Room, Message, MutedUser]),UserModule, AuthModule, FriendModule, PongModule],
   providers: [SocketGateway, GlobalGateway, RoomService],
 })
 export class SocketModule {}
