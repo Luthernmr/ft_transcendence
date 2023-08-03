0d6c39391423d9ec8593d9064cf12a181f31f36e
nemorluther@gmail.com <nemorluther@gmail.com>
2023-08-03 12:41:29 +0200

    select room

--- frontend/src/components/Chat/RoomList.tsx
+++ frontend/src/components/Chat/RoomList.tsx
@@ -1,275 +1,291 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import {
-  Box,
-  Flex,
-  Heading,
-  Avatar,
-  AvatarGroup,
-  VStack,
-  IconButton,
-  StackDivider,
-  Text,
-  HStack,
-  Spacer,
-  Input,
-  Button,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useToast,
+	Box,
+	Flex,
+	Heading,
+	Avatar,
+	AvatarGroup,
+	VStack,
+	IconButton,
+	StackDivider,
+	Text,
+	HStack,
+	Spacer,
+	Input,
+	Button,
+	useDisclosure,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useToast,
 } from "@chakra-ui/react";
 import {
-  AddIcon,
-  DeleteIcon,
-  LockIcon,
-  ViewIcon,
-  ViewOffIcon,
+	AddIcon,
+	DeleteIcon,
+	LockIcon,
+	ViewIcon,
+	ViewOffIcon,
 } from "@chakra-ui/icons";
 import { chatSocket } from "../../sockets/sockets";
 import { User } from "../Social/AllUserItem";
 import { Room } from "./ChatRoom";
 
 interface RoomListProps {
-  setSelectedRoom: (room: Room) => void;
-  setShowCreateRoom: (show: boolean) => void;
+	setSelectedRoom: (room: Room) => void;
+	setShowCreateRoom: (show: boolean) => void;
 }
 
 const RoomList: React.FC<RoomListProps> = ({
-  setSelectedRoom,
-  setShowCreateRoom,
+	setSelectedRoom,
+	setShowCreateRoom,
 }) => {
-  const currentUser = JSON.parse(sessionStorage.getItem("currentUser") || "{}");
-  const [rooms, setRooms] = useState<Room[]>([]);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [roomPassword, setRoomPassword] = useState("");
-  const [selectedRoomLocal, setSelectedRoomLocal] = useState<Room | null>(null);
-  const toast = useToast();
+	const currentUser = JSON.parse(sessionStorage.getItem("currentUser") || "{}");
+	const [rooms, setRooms] = useState<Room[]>([]);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [roomPassword, setRoomPassword] = useState("");
+	const [selectedRoomLocal, setSelectedRoomLocal] = useState<Room | null>(null);
+	const toast = useToast();
 
-  const handleRoomClick = (room: Room) => {
-    setSelectedRoomLocal(null);
-    if (room.password) {
-      setSelectedRoomLocal(room);
-      onOpen();
-    } else {
-      chatSocket.emit("joinRoom", { userId: currentUser.id, room: room });
-      chatSocket.on("joinedRoom", (joinedRoom: Room) => {
-        setSelectedRoom(joinedRoom);
-      });
-    }
-  };
+	const handleRoomClick = (room: Room) => {
+		setSelectedRoomLocal(null);
+		if (room.password) {
 
-  const handleDeleteRoom = (e: FormEvent, room: Room) => {
-    e.stopPropagation();
-    e.preventDefault();
-    chatSocket.emit("deleteRoom", room);
-  };
+			console.log('setSelectedroom local  name', room.name )
+			setSelectedRoomLocal(room);
+			onOpen();
+		} else {
+			chatSocket.emit("joinRoom", { userId: currentUser.id, room: room });
+			chatSocket.on("joinedRoom", (joinedRoom: Room) => {
+				setSelectedRoom(joinedRoom);
+			});
+		}
+	};
 
-  const handlePasswordSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    chatSocket.emit("checkRoomPassword", {
-      room: selectedRoomLocal,
-      password: roomPassword,
-    });
-    chatSocket.on("passCheck", (check: boolean) => {
-      if (check && selectedRoomLocal) {
-        chatSocket.emit("joinRoom", {
-          userId: currentUser.id,
-          room: selectedRoomLocal,
-        });
-        chatSocket.on("joinedRoom", (joinedRoom: Room) => {
-          setSelectedRoom(joinedRoom);
-          setSelectedRoomLocal(null);
-        });
-      } else {
-        setRoomPassword("");
-        setSelectedRoomLocal(null);
-        return;
-      }
-    });
-    onClose();
-  };
+	const handleDeleteRoom = (e: FormEvent, room: Room) => {
+		e.stopPropagation();
+		e.preventDefault();
+		chatSocket.emit("deleteRoom", room);
+	};
 
-  function handleRoomList(rooms: Room[]) {
-    setRooms(rooms);
-  }
+	useEffect(() => {
+		chatSocket.on("passCheck", (check: boolean) => {
+			
+			if (check && selectedRoomLocal) {
+			console.log('setSelectedroom local  pass Check name', selectedRoomLocal.name )
 
-  async function handleRoomDeleted(roomName: string) {
-    toast({
-      title: roomName + " room deleted",
-      status: "info",
-      isClosable: true,
-      position: "top",
-    });
-    chatSocket.emit("getUserRooms", { userId: currentUser.id });
-  }
+				chatSocket.emit("joinRoom", {
+					userId: currentUser.id,
+					room: selectedRoomLocal,
+				});
 
-  function handleRoomCreated() {
-    chatSocket.emit("getUserRooms", { userId: currentUser.id });
-  }
+			} else {
+				setRoomPassword("");
+				setSelectedRoomLocal(null);
+				return;
+			}
+		});
 
-  function handleUpdatedRoom() {
-    chatSocket.emit("getUserRooms", { userId: currentUser.id });
-  }
+		chatSocket.on("joinedRoom", (joinedRoom: Room) => {
+			setSelectedRoom(joinedRoom);
+			setSelectedRoomLocal(null);
+			onClose();
+		});
+		return () => {
+			chatSocket.off("joinedRoom")
+			chatSocket.off("passCheck")
+		}	
+	})
 
-  useEffect(() => {
-    chatSocket.emit("getUserRooms", { userId: currentUser.id });
-    chatSocket.on("roomList", handleRoomList);
-    chatSocket.on("roomDeleted", handleRoomDeleted);
-    chatSocket.on("roomCreated", handleRoomCreated);
-    chatSocket.on("updatedRoom", handleUpdatedRoom);
+	const handlePasswordSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		chatSocket.emit("checkRoomPassword", {
+			room: selectedRoomLocal,
+			password: roomPassword,
+		});
 
-    return () => {
-      chatSocket.off("roomList", handleRoomList);
-      chatSocket.off("roomDeleted", handleRoomDeleted);
-      chatSocket.off("roomCreated", handleRoomCreated);
-      chatSocket.off("updatedRoom", handleUpdatedRoom);
-    };
-  }, [currentUser.id]);
 
-  return (
-    <Flex
-      borderRadius={"md"}
-      bg={"white"}
-      padding={"15px"}
-      minHeight={"100%"}
-      flex={"1"}
-      direction={"column"}
-      maxH={"100%"}
-    >
-      <Flex justifyContent={"space-between"} alignItems={"center"} mb={4}>
-        <IconButton
-          aria-label={"Add room"}
-          icon={<AddIcon />}
-          onClick={() => setShowCreateRoom(true)}
-        />
-        <Heading size={"md"} textAlign={"center"} flex={"1"}>
-          Rooms
-        </Heading>
-      </Flex>
-      <VStack
-        divider={<StackDivider borderColor="gray.200" />}
-        spacing={4}
-        align="stretch"
-        height="100%"
-        overflowY="auto"
-      >
-        {rooms?.length > 0 ? (
-          rooms.map((room, index) => (
-            <Box
-              flex-direction={"column"}
-              p={2}
-              borderRadius={"8"}
-              key={index}
-              onClick={() => {
-                if (
-                  !room.bannedUsers ||
-                  !room.bannedUsers.find((user) => user.id === currentUser.id)
-                ) {
-                  handleRoomClick(room);
-                }
-              }}
-              _hover={{
-				cursor : 'pointer',
-                bg:
-                  room.bannedUsers &&
-                  room.bannedUsers.find((user) => user.id === currentUser.id)
-                    ? "none"
-                    : "gray.200",
-              }}
-              style={
-                room.bannedUsers &&
-                room.bannedUsers.find((user) => user.id === currentUser.id)
-                  ? {
-                      backgroundColor: "gray",
-                      opacity: 0.5,
-                      pointerEvents: "none",
-                    }
-                  : {}
-              }
-            >
-              <HStack>
-                <AvatarGroup size={"md"} max={3}>
-                  {room?.users?.map((user: User) => (
-                    <Avatar
-                      name={user.nickname}
-                      src={user.imgPdp}
-                      key={user.id}
-                    />
-                  ))}
-                </AvatarGroup>
-                <Spacer />
-                <Text mr={2}>{room.name}</Text>
-                {room.isPrivate && (
-                  <ViewOffIcon boxSize={6} ml={2} color={"gray.500"} />
-                )}
-                {room.password && (
-                  <LockIcon boxSize={6} ml={2} color={"gray.500"} />
-                )}
-                {!room.isPrivate && !room.password && (
-                  <ViewIcon boxSize={6} ml={2} color={"gray.500"} />
-                )}
-                {currentUser.id === room.ownerId ? (
-                  <IconButton
-                    icon={<DeleteIcon />}
-                    _hover={{ color: "red" }}
-                    color="gray"
-                    bg={"none"}
-                    aria-label="Call Segun"
-                    size="lg"
-                    onClick={(e) => handleDeleteRoom(e, room)}
-                  />
-                ) : (
-                  <IconButton
-                    _hover={{ color: "none" }}
-                    color="none"
-                    bg={"none"}
-                    aria-label="Hello ;)"
-                    size="lg"
-                  />
-                )}
-              </HStack>
-            </Box>
-          ))
-        ) : (
-          <Text textAlign={"center"} width="100%">
-            No rooms available. Please create a new one.
-          </Text>
-        )}
-      </VStack>
+	};
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Password Required</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <form onSubmit={handlePasswordSubmit}>
-              <Input
-                type="password"
-                placeholder="Enter password"
-                value={roomPassword}
-                onChange={(e) => setRoomPassword(e.target.value)}
-              />
-            </form>
-          </ModalBody>
+	function handleRoomList(rooms: Room[]) {
+		setRooms(rooms);
+	}
 
-          <ModalFooter>
-            <Button variant="ghost" mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue" onClick={handlePasswordSubmit}>
-              Submit
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Flex>
-  );
+	async function handleRoomDeleted(roomName: string) {
+		toast({
+			title: roomName + " room deleted",
+			status: "info",
+			isClosable: true,
+			position: "top",
+		});
+		chatSocket.emit("getUserRooms", { userId: currentUser.id });
+	}
+
+	function handleRoomCreated() {
+		chatSocket.emit("getUserRooms", { userId: currentUser.id });
+	}
+
+	function handleUpdatedRoom() {
+		chatSocket.emit("getUserRooms", { userId: currentUser.id });
+	}
+
+	useEffect(() => {
+		chatSocket.emit("getUserRooms", { userId: currentUser.id });
+		chatSocket.on("roomList", handleRoomList);
+		chatSocket.on("roomDeleted", handleRoomDeleted);
+		chatSocket.on("roomCreated", handleRoomCreated);
+		chatSocket.on("updatedRoom", handleUpdatedRoom);
+
+		return () => {
+			chatSocket.off("roomList", handleRoomList);
+			chatSocket.off("roomDeleted", handleRoomDeleted);
+			chatSocket.off("roomCreated", handleRoomCreated);
+			chatSocket.off("updatedRoom", handleUpdatedRoom);
+		};
+	}, [currentUser.id]);
+
+	return (
+		<Flex
+			borderRadius={"md"}
+			bg={"white"}
+			padding={"15px"}
+			minHeight={"100%"}
+			flex={"1"}
+			direction={"column"}
+			maxH={"100%"}
+		>
+			<Flex justifyContent={"space-between"} alignItems={"center"} mb={4}>
+				<IconButton
+					aria-label={"Add room"}
+					icon={<AddIcon />}
+					onClick={() => setShowCreateRoom(true)}
+				/>
+				<Heading size={"md"} textAlign={"center"} flex={"1"}>
+					Rooms
+				</Heading>
+			</Flex>
+			<VStack
+				divider={<StackDivider borderColor="gray.200" />}
+				spacing={4}
+				align="stretch"
+				height="100%"
+				overflowY="auto"
+			>
+				{rooms?.length > 0 ? (
+					rooms.map((room, index) => (
+						<Box
+							flex-direction={"column"}
+							p={2}
+							borderRadius={"8"}
+							key={index}
+							onClick={() => {
+								if (
+									!room.bannedUsers ||
+									!room.bannedUsers.find((user) => user.id === currentUser.id)
+								) {
+									handleRoomClick(room);
+								}
+							}}
+							_hover={{
+								cursor: 'pointer',
+								bg:
+									room.bannedUsers &&
+										room.bannedUsers.find((user) => user.id === currentUser.id)
+										? "none"
+										: "gray.200",
+							}}
+							style={
+								room.bannedUsers &&
+									room.bannedUsers.find((user) => user.id === currentUser.id)
+									? {
+										backgroundColor: "gray",
+										opacity: 0.5,
+										pointerEvents: "none",
+									}
+									: {}
+							}
+						>
+							<HStack>
+								<AvatarGroup size={"md"} max={3}>
+									{room.users?.map((user: User) => (
+										<Avatar
+											name={user.nickname}
+											src={user.imgPdp}
+											key={user.id}
+										/>
+									))}
+								</AvatarGroup>
+								<Spacer />
+								<Text mr={2}>{room.name}</Text>
+								{room.isPrivate && (
+									<ViewOffIcon boxSize={6} ml={2} color={"gray.500"} />
+								)}
+								{room.password && (
+									<LockIcon boxSize={6} ml={2} color={"gray.500"} />
+								)}
+								{!room.isPrivate && !room.password && (
+									<ViewIcon boxSize={6} ml={2} color={"gray.500"} />
+								)}
+								{currentUser.id === room.ownerId ? (
+									<IconButton
+										icon={<DeleteIcon />}
+										_hover={{ color: "red" }}
+										color="gray"
+										bg={"none"}
+										aria-label="Call Segun"
+										size="lg"
+										onClick={(e) => handleDeleteRoom(e, room)}
+									/>
+								) : (
+									<IconButton
+										_hover={{ color: "none" }}
+										color="none"
+										bg={"none"}
+										aria-label="Hello ;)"
+										size="lg"
+									/>
+								)}
+							</HStack>
+						</Box>
+					))
+				) : (
+					<Text textAlign={"center"} width="100%">
+						No rooms available. Please create a new one.
+					</Text>
+				)}
+			</VStack>
+
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>Password Required</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<form onSubmit={handlePasswordSubmit}>
+							<Input
+								type="password"
+								placeholder="Enter password"
+								value={roomPassword}
+								onChange={(e) => setRoomPassword(e.target.value)}
+							/>
+						</form>
+					</ModalBody>
+
+					<ModalFooter>
+						<Button variant="ghost" mr={3} onClick={onClose}>
+							Cancel
+						</Button>
+						<Button colorScheme="blue" onClick={handlePasswordSubmit}>
+							Submit
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</Flex>
+	);
 };
 
 export default RoomList;
